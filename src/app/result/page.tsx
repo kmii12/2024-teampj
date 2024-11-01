2e3302478a37a5a3210a578aaa0f387f43985755
snhrayk <ayaka21sunahara@gmail.com>
2024-11-01 09:43:25 +0900

    作成中

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,100 +1,207 @@
+// "use client";
+
+// import Link from "next/link";
+// import styles from "./Result.module.scss";
+// // import { usePathname } from "next/navigation";
+// import SearchBar from "@/app/components/SearchBar";
+// import Header from "@/app/components/Header";
+
+// export default function Result() {
+//   // const pathname = usePathname();
+
+//   //ダミーデータ（あとからfirebaseから取得）
+//   const ResultDatas = [
+//     {
+//       id: 1,
+//       title: "桃太郎",
+//       writer: "不明",
+//       illustrator: "不明",
+//       image: "/img/momotaro.jpeg",
+//       livingThing: ["人間", "陸の生き物", "空の生き物"],
+//       mainCharacter: "桃太郎",
+//       character: [
+//         "桃太郎",
+//         "犬",
+//         "猿",
+//         "キジ",
+//         "鬼",
+//         "おじいさん",
+//         "おばあさん",
+//       ],
+//       genre: "昔話",
+//       location: "日本",
+//       hue: "不明",
+//       atmosphere: "",
+//       season: "不明",
+//     },
+//     {
+//       id: 2,
+//       title: "赤ずきん",
+//       writer: "不明",
+//       illustrator: "不明",
+//       image: "/img/littleRedRidingHood.jpeg",
+//       livingThing: ["人間", "陸の生き物", "空の生き物"],
+//       mainCharacter: "赤ずきん",
+//       character: ["赤ずきん", "おばあさん", "オオカミ"],
+//       hue: "不明",
+//       atmosphere: ["かわいい", "怖い"],
+//       genre: "昔話",
+//       location: "海外",
+//       season: "不明",
+//     },
+//     {
+//       id: 3,
+//       title: "シンデレラ",
+//       writer: "不明",
+//       illustrator: "不明",
+//       image: "/img/cinderella.jpeg",
+//       livingThing: "人間",
+//       mainCharacter: "シンデレラ",
+//       character: ["シンデレラ", "王子", "魔法使い"],
+//       genre: "昔話",
+//       location: "海外",
+//       hue: "不明",
+//       atmosphere: "",
+//       season: "不明",
+//     },
+//     {
+//       id: 4,
+//       title: "白雪姫",
+//       writer: "不明",
+//       illustrator: "不明",
+//       image: "/img/snowWhite.jpeg",
+//       livingThing: ["人間", "陸の生き物"],
+//       mainCharacter: "白雪姫",
+//       character: ["白雪姫", "王子", "魔女", "小人"],
+//       genre: "昔話",
+//       location: "海外",
+//       hue: "不明",
+//       atmosphere: "",
+//       season: "不明",
+//     },
+//     {
+//       id: 5,
+//       title: "アラジンと魔法のランプ",
+//       writer: "不明",
+//       illustrator: "不明",
+//       image: "/img/aladdin.jpeg",
+//       livingThing: ["人間", "陸の生き物"],
+//       mainCharacter: "アラジン",
+//       character: ["アラジン", "ジーニー", "王女", "魔法使い", "盗賊"],
+//       genre: "昔話",
+//       location: "海外",
+//       hue: "不明",
+//       atmosphere: "",
+//       season: "不明",
+//     },
+//   ];
+//   return (
+//     <>
+//       <Header />
+//       <div className={styles.searchBarWrap}>
+//         <SearchBar />
+//       </div>
+//       <main className={styles.resultContainer}>
+//         <h2 className={styles.resultTitle}>検索結果({ResultDatas.length}件)</h2>
+//         <ul className={styles.resultList}>
+//           <div className={styles.resultItemWrap}>
+//             {ResultDatas.map((result) => (
+//               <li key={result.id} className={styles.resultItem}>
+//                 <Link href={`/result/resultDetail/${result.id}`}>
+//                   <div className={styles.resultItemWrap}>
+//                     <div className={styles.itemImageWrap}>
+//                       <img
+//                         src={result.image}
+//                         alt={result.title}
+//                         className={styles.image}
+//                       />
+//                     </div>
+//                     <div className={styles.itemDescriptionWrap}>
+//                       <h3 className="title">題名　{result.title}</h3>
+//                       <div className={styles.fixedBottom}>
+//                         <p className="writer">作者　{result.writer}</p>
+//                         <p className="illustrator">絵　{result.illustrator}</p>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </Link>
+//               </li>
+//             ))}
+//           </div>
+//         </ul>
+//       </main>
+//     </>
+//   );
+// }
+
 "use client";
 
-import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { PictureBook } from "../types/pictureBook";
+import data from "../data/pictureBook.json";
 import styles from "./Result.module.scss";
-// import { usePathname } from "next/navigation";
-import SearchBar from "@/app/components/SearchBar";
-import Header from "@/app/components/Header";
+import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Result() {
-  // const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [filteredBooks, setFilteredBooks] = useState<PictureBook[]>([]);
 
-  //ダミーデータ（あとからfirebaseから取得）
-  const ResultDatas = [
-    {
-      id: 1,
-      title: "桃太郎",
-      writer: "不明",
-      illustrator: "不明",
-      image: "/img/momotaro.jpeg",
-      livingThing: ["人間", "陸の生き物", "空の生き物"],
-      mainCharacter: "桃太郎",
-      character: [
-        "桃太郎",
-        "犬",
-        "猿",
-        "キジ",
-        "鬼",
-        "おじいさん",
-        "おばあさん",
-      ],
-      genre: "昔話",
-      location: "日本",
-      hue: "不明",
-      atmosphere: "",
-      season: "不明",
-    },
-    {
-      id: 2,
-      title: "赤ずきん",
-      writer: "不明",
-      illustrator: "不明",
-      image: "/img/littleRedRidingHood.jpeg",
-      livingThing: ["人間", "陸の生き物", "空の生き物"],
-      mainCharacter: "赤ずきん",
-      character: ["赤ずきん", "おばあさん", "オオカミ"],
-      hue: "不明",
-      atmosphere: ["かわいい", "怖い"],
-      genre: "昔話",
-      location: "海外",
-      season: "不明",
-    },
-    {
-      id: 3,
-      title: "シンデレラ",
-      writer: "不明",
-      illustrator: "不明",
-      image: "/img/cinderella.jpeg",
-      livingThing: "人間",
-      mainCharacter: "シンデレラ",
-      character: ["シンデレラ", "王子", "魔法使い"],
-      genre: "昔話",
-      location: "海外",
-      hue: "不明",
-      atmosphere: "",
-      season: "不明",
-    },
-    {
-      id: 4,
-      title: "白雪姫",
-      writer: "不明",
-      illustrator: "不明",
-      image: "/img/snowWhite.jpeg",
-      livingThing: ["人間", "陸の生き物"],
-      mainCharacter: "白雪姫",
-      character: ["白雪姫", "王子", "魔女", "小人"],
-      genre: "昔話",
-      location: "海外",
-      hue: "不明",
-      atmosphere: "",
-      season: "不明",
-    },
-    {
-      id: 5,
-      title: "アラジンと魔法のランプ",
-      writer: "不明",
-      illustrator: "不明",
-      image: "/img/aladdin.jpeg",
-      livingThing: ["人間", "陸の生き物"],
-      mainCharacter: "アラジン",
-      character: ["アラジン", "ジーニー", "王女", "魔法使い", "盗賊"],
-      genre: "昔話",
-      location: "海外",
-      hue: "不明",
-      atmosphere: "",
-      season: "不明",
-    },
-  ];
+  useEffect(() => {
+    const selectedKeywords = JSON.parse(
+      searchParams.get("selectedKeywords") || "{}"
+    );
+
+    // 絞り込みロジック：各キーワードカテゴリーに基づき、データをフィルタリング
+    const filteredBooksData = data.filter((book: PictureBook) => {
+      const matchesSeason =
+        selectedKeywords.seasons.length === 0 ||
+        selectedKeywords.seasons.includes(book.season);
+
+      const matchesLivingThing =
+        selectedKeywords.livingThings.length === 0 ||
+        (Array.isArray(book.livingThing) &&
+          book.livingThing.length > 0 &&
+          book.livingThing.some((thing) =>
+            selectedKeywords.livingThings.includes(thing)
+          ));
+
+      const matchesHue =
+        selectedKeywords.hue.length === 0 ||
+        selectedKeywords.hue.includes(book.hue);
+
+      const matchesAtmosphere =
+        selectedKeywords.atmosphere.length === 0 ||
+        (Array.isArray(book.atmosphere) &&
+          book.atmosphere.length > 0 &&
+          book.atmosphere.some((atm) =>
+            selectedKeywords.atmosphere.includes(atm)
+          ));
+
+      const matchesLocation =
+        selectedKeywords.locations.length === 0 ||
+        selectedKeywords.locations.includes(book.location);
+
+      const matchesFeature =
+        selectedKeywords.features.length === 0 ||
+        selectedKeywords.features.includes(book.feature);
+
+      return (
+        matchesSeason &&
+        matchesLivingThing &&
+        matchesHue &&
+        matchesAtmosphere &&
+        matchesLocation &&
+        matchesFeature
+      );
+    });
+
+    setFilteredBooks(filteredBooksData);
+  }, [searchParams]);
+
   return (
     <>
       <Header />
@@ -102,18 +209,22 @@ export default function Result() {
         <SearchBar />
       </div>
       <main className={styles.resultContainer}>
-        <h2 className={styles.resultTitle}>検索結果({ResultDatas.length}件)</h2>
+        <h2 className={styles.resultTitle}>
+          検索結果({filteredBooks.length}件)
+        </h2>
         <ul className={styles.resultList}>
           <div className={styles.resultItemWrap}>
-            {ResultDatas.map((result) => (
+            {filteredBooks.map((result) => (
               <li key={result.id} className={styles.resultItem}>
                 <Link href={`/result/resultDetail/${result.id}`}>
                   <div className={styles.resultItemWrap}>
                     <div className={styles.itemImageWrap}>
-                      <img
+                      <Image
                         src={result.image}
                         alt={result.title}
                         className={styles.image}
+                        width={200}
+                        height={200}
                       />
                     </div>
                     <div className={styles.itemDescriptionWrap}>
