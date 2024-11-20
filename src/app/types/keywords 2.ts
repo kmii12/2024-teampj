710a15bad2888d8807409d2b77f044406bec706e
mk <kanzakimiki.12@gmail.com>
2024-11-21 01:17:30 +0900

    db設定

---
+++ src/app/types/keywords 2.ts
@@ -0,0 +1,9 @@
+// types/Keywords.ts
+export interface Keywords {
+  seasons: string[];
+  livingThings: string[];
+  hue: string[];
+  atmosphere: string[];
+  locations: string[];
+  features: string[];
+}
