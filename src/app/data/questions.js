92461df39f8f9e9792ff8b14fe45b27f45b84809
snhrayk <ayaka21sunahara@gmail.com>
2024-12-06 00:15:18 +0900

    作成中

---
+++ src/app/data/questions.js
@@ -0,0 +1,23 @@
+export const questions = [
+  {
+    id: 1,
+    question: "Q1.絵本のタイプを選んでください",
+    options: [
+      { label: "シンプルな物語", value: "type_story" },
+      { label: "間違い探し", value: "type_puzzle" },
+      { label: "布の絵本", value: "type_cloth" },
+      { label: "仕掛け絵本", value: "type_trick" },
+      { label: "形が特殊な絵本", value: "type_shape" },
+      { label: "迷路", value: "type_maze" },
+    ],
+  },
+  // {
+  //   id: 2,
+  //   question: "対象年齢を選んでください",
+  //   options: [
+  //     { label: "0〜3歳", value: "age_0_3" },
+  //     { label: "4〜6歳", value: "age_4_6" },
+  //     { label: "7歳以上", value: "age_7_plus" },
+  //   ],
+  // },
+];
