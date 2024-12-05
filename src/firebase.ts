8306dff7097a0b03279a773bc5d06f7daf7611b7
mk <kanzakimiki.12@gmail.com>
2024-12-06 02:37:34 +0900

    修正

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,15 +8,19 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_APP_FIREBASE_KEY,
-  authDomain: process.env.NEXT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.NEXT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.NEXT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_APP_FIREBASE_SENDERID,
-  appId: process.env.NEXT_APP_FIREBASE_APPID,
-  measurementId: process.env.NEXT_APP_FIREBASE_MEASUREMENTID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Firestoreを初期化
+const db = getFirestore(app);
+export { app, db, analytics };
