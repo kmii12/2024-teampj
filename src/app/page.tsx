86368421ad5dddf396fb8429e572ed48a0660c04
snhrayk <ayaka21sunahara@gmail.com>
2024-12-22 16:54:10 +0900

    firebase設定やり直し中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,119 +1,119 @@
-"use client";
-import styles from "./page.module.scss";
-import Header from "./components/Header";
-import Link from "next/link";
-import { questions } from "./data/questions";
-import { collection, getDocs, query } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { db } from "@/firebase";
+// "use client";
+// import styles from "./page.module.scss";
+// import Header from "./components/Header";
+// import Link from "next/link";
+// import { questions } from "./data/questions";
+// import { collection, getDocs, query } from "firebase/firestore";
+// import { useEffect, useState } from "react";
+// import { db } from "@/firebase";
 
-export default function Home() {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // 現在の質問インデックス
-  const [selectedOptions, setSelectedOptions] = useState<string[]>([]); // 選択されたオプションを管理
-  // const [bookCount, setBookCount] = useState<number>(0); // 見つかった絵本の冊数
+// export default function Home() {
+//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // 現在の質問インデックス
+//   const [selectedOptions, setSelectedOptions] = useState<string[]>([]); // 選択されたオプションを管理
+//   // const [bookCount, setBookCount] = useState<number>(0); // 見つかった絵本の冊数
 
-  // 現在の質問データ
-  const currentQuestion = questions[currentQuestionIndex];
+//   // 現在の質問データ
+//   const currentQuestion = questions[currentQuestionIndex];
 
-  // Firestoreから絵本のデータを取得
-  const fetchBooks = async () => {
-    try {
-      const booksCollection = collection(db, "picturebooks");
-      const q = query(booksCollection); // クエリの条件を追加する場合
+//   // Firestoreから絵本のデータを取得
+//   const fetchBooks = async () => {
+//     try {
+//       const booksCollection = collection(db, "picturebooks");
+//       const q = query(booksCollection); // クエリの条件を追加する場合
 
-      const querySnapshot = await getDocs(q);
-      const books = querySnapshot.docs.map((doc) => doc.data());
-      console.log(books); // 取得したデータをログに出力
+//       const querySnapshot = await getDocs(q);
+//       const books = querySnapshot.docs.map((doc) => doc.data());
+//       console.log(books); // 取得したデータをログに出力
 
-      // setBookCount(querySnapshot.size); // 絵本の冊数を保存
-    } catch (error) {
-      console.error("Firestoreからデータを取得できませんでした:", error);
-    }
-  };
+//       // setBookCount(querySnapshot.size); // 絵本の冊数を保存
+//     } catch (error) {
+//       console.error("Firestoreからデータを取得できませんでした:", error);
+//     }
+//   };
 
-  console.log(db);
+//   console.log(db);
 
-  // ページロード時にFirestoreデータを取得
-  useEffect(() => {
-    // const fetchData = async () => {
-    //   const querySnapshot = await getDocs(collection(db, "picturebooks"));
-    //   console.log(querySnapshot);
+//   // ページロード時にFirestoreデータを取得
+//   useEffect(() => {
+//     // const fetchData = async () => {
+//     //   const querySnapshot = await getDocs(collection(db, "picturebooks"));
+//     //   console.log(querySnapshot);
 
-    //   const data = querySnapshot.docs.map((doc) => ({
-    //     id: doc.id,
-    //     ...doc.data(),
-    //   }));
+//     //   const data = querySnapshot.docs.map((doc) => ({
+//     //     id: doc.id,
+//     //     ...doc.data(),
+//     //   }));
 
-    //   console.log("絵本データ:", data);
-    // };
-    // fetchData();
-    fetchBooks();
-  }, []);
+//     //   console.log("絵本データ:", data);
+//     // };
+//     // fetchData();
+//     fetchBooks();
+//   }, []);
 
-  // オプション選択時の処理
-  const handleOptionSelect = (value: string) => {
-    const updatedOptions = [...selectedOptions];
-    updatedOptions[currentQuestionIndex] = value;
-    setSelectedOptions(updatedOptions);
-  };
+//   // オプション選択時の処理
+//   const handleOptionSelect = (value: string) => {
+//     const updatedOptions = [...selectedOptions];
+//     updatedOptions[currentQuestionIndex] = value;
+//     setSelectedOptions(updatedOptions);
+//   };
 
-  // 次の質問へ進む
-  const handleNext = () => {
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-    }
-  };
+//   // 次の質問へ進む
+//   const handleNext = () => {
+//     if (currentQuestionIndex < questions.length - 1) {
+//       setCurrentQuestionIndex(currentQuestionIndex + 1);
+//     }
+//   };
 
-  // 前の質問に戻る
-  const handleBack = () => {
-    if (currentQuestionIndex > 0) {
-      setCurrentQuestionIndex(currentQuestionIndex - 1);
-    }
-  };
+//   // 前の質問に戻る
+//   const handleBack = () => {
+//     if (currentQuestionIndex > 0) {
+//       setCurrentQuestionIndex(currentQuestionIndex - 1);
+//     }
+//   };
 
-  return (
-    <>
-      <Header />
-      <div className={styles.questionWrap}>
-        <div className={styles.question}>
-          <p>{currentQuestion.question}</p>
-        </div>
-        <div className={styles.cat}></div>
-      </div>
-      <div className={styles.answerWrap}>
-        <ul>
-          {currentQuestion.options.map((option) => (
-            <li
-              key={option.value}
-              onClick={() => handleOptionSelect(option.value)}
-              className={
-                selectedOptions[currentQuestionIndex] === option.value
-                  ? styles.selected
-                  : ""
-              }
-            >
-              {option.label}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className={styles.btnWrap}>
-        <button onClick={handleBack} disabled={currentQuestionIndex === 0}>
-          戻る
-        </button>
-        <button>
-          <Link href="/result">
-            {/* <span>見つかった絵本({bookCount}冊)</span>検索 */}
-            <span>見つかった絵本(5冊)</span>検索
-          </Link>
-        </button>
-        <button
-          onClick={handleNext}
-          disabled={currentQuestionIndex === questions.length - 1}
-        >
-          次へ
-        </button>
-      </div>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Header />
+//       <div className={styles.questionWrap}>
+//         <div className={styles.question}>
+//           <p>{currentQuestion.question}</p>
+//         </div>
+//         <div className={styles.cat}></div>
+//       </div>
+//       <div className={styles.answerWrap}>
+//         <ul>
+//           {currentQuestion.options.map((option) => (
+//             <li
+//               key={option.value}
+//               onClick={() => handleOptionSelect(option.value)}
+//               className={
+//                 selectedOptions[currentQuestionIndex] === option.value
+//                   ? styles.selected
+//                   : ""
+//               }
+//             >
+//               {option.label}
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//       <div className={styles.btnWrap}>
+//         <button onClick={handleBack} disabled={currentQuestionIndex === 0}>
+//           戻る
+//         </button>
+//         <button>
+//           <Link href="/result">
+//             {/* <span>見つかった絵本({bookCount}冊)</span>検索 */}
+//             <span>見つかった絵本(5冊)</span>検索
+//           </Link>
+//         </button>
+//         <button
+//           onClick={handleNext}
+//           disabled={currentQuestionIndex === questions.length - 1}
+//         >
+//           次へ
+//         </button>
+//       </div>
+//     </>
+//   );
+// }
