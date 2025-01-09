f1b6e62dbbf9bdeef550a430e222d749688bc4ed
mk <kanzakimiki.12@gmail.com>
2025-01-09 15:05:07 +0900

    savedBranch Header追加

--- src/app/saved/page.tsx
+++ src/app/saved/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 
+import Header from "../components/Header";
+// import SearchBar from "./components/SearchBar";
+import styles from "./saved.module.scss";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+//firebase
+import { db } from "@/firebase";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
 export default function Saved() {
   return (
     <>
+      <Header />
       <h1>保存した絵本</h1>
     </>
   );
