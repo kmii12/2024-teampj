419c52ed67007f01dbe7bf71c7ba04c9b0c5d433
mk <kanzakimiki.12@gmail.com>
2025-01-14 10:12:20 +0900

    sass 警告を無視

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ビルド時にESLintエラーを無視
   },
+  sassOptions: {
+    warnOnly: true, // Sass の警告をエラーとして扱わない
+  },
 };
 
 export default nextConfig;
