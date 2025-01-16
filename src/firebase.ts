2a62974b20fbddb2f8579ba1727ff005f83fec64
mk <kanzakimiki.12@gmail.com>
2025-01-16 11:59:21 +0900

    edit

--- src/firebase.ts
+++ src/firebase.ts
@@ -20,5 +20,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+console.log(db);
+
 // const analytics = getAnalytics(app);
 export { app, db };
