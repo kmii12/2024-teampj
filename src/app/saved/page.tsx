2f70c6b9ec4324e2c253042aeb3930fe39429828
mk <kanzakimiki.12@gmail.com>
2025-01-17 03:44:26 +0900

    edit

--- src/app/saved/page.tsx
+++ src/app/saved/page.tsx
@@ -1,20 +1,113 @@
+// "use client";
+
+// import Header from "../components/Header";
+// // import SearchBar from "./components/SearchBar";
+// import styles from "./saved.module.scss";
+// import { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
+
+// //firebase
+// import { db } from "@/firebase";
+// import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+// export default function Saved() {
+//   return (
+//     <>
+//       <Header />
+//       <h1 className={styles.title}>保存</h1>
+//     </>
+//   );
+// }
+
 "use client";
 
-import Header from "../components/Header";
-// import SearchBar from "./components/SearchBar";
+import Link from "next/link";
 import styles from "./saved.module.scss";
+// import { usePathname } from "next/navigation";
+import SearchBar from "@/app/components/SearchBar";
+import Header from "@/app/components/Header";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import Image from "next/image";
 
-//firebase
+//firesbase
 import { db } from "@/firebase";
+
+// console.log(db);
+
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 
-export default function Saved() {
+interface PictureBook {
+  id: string;
+  title: string;
+  writer: string;
+  image: string;
+  mainCharacter: string;
+  character: string[];
+  genre: string;
+  location: string;
+  atmosphere: string[];
+}
+
+export default function SavedList() {
+  //firebaseの絵本データ
+  const [savedDatas, setSavedDates] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      //dbに picturebooksのコレクションを設定
+      const querySnapshot = await getDocs(collection(db, "picturebooks"));
+      // console.log(querySnapshot);
+      //dataに配列として挿入
+      const data = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log("絵本データ:", data);
+      setSavedDates(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <Header />
-      <h1>保存した絵本</h1>
+      <div className={styles.searchBarWrap}>
+        <SearchBar />
+      </div>
+      <main className={styles.resultContainer}>
+        <h2 className={styles.resultTitle}>検索結果({savedDatas.length}件)</h2>
+        <ul className={styles.resultList}>
+          <div className={styles.resultItemWrap}>
+            {savedDatas.map((savedItem) => (
+              <li key={savedItem.id} className={styles.resultItem}>
+                <Link href={`/result/resultDetail/${savedItem.id}`}>
+                  <div className={styles.resultItemWrap}>
+                    <div className={styles.itemImageWrap}>
+                      <div className={styles.imgWrap}>
+                        <Image
+                          src={savedItem.img}
+                          alt={savedItem.title}
+                          width={200}
+                          height={200}
+                          className={styles.image}
+                        />
+                      </div>
+                    </div>
+
+                    <div className={styles.itemDescriptionWrap}>
+                      <h3 className="title">{savedItem.title}</h3>
+                      <div className={styles.summaryContainer}>
+                        <h4>あらすじ</h4>
+                        <p className={styles.summary}>{savedItem.summary}</p>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </div>
+        </ul>
+      </main>
     </>
   );
 }
